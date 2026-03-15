4ce32cd7cada80332ec096563c04ea1545fad824
[name] <maheshbabu02456@gmail.com>
2026-03-15 10:39:19 +0530

    Initial commit (Cleaned history & Secured secrets)

--- frontend/src/firebase.config.js
+++ frontend/src/firebase.config.js
@@ -4,14 +4,15 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCNUzgTmhqscHUcKYZWOT35pN6k9R3GrOo",
-  authDomain: "visiontalk-73e39.firebaseapp.com",
-  projectId: "visiontalk-73e39",
-  storageBucket: "visiontalk-73e39.firebasestorage.app",
-  messagingSenderId: "956846294687",
-  appId: "1:956846294687:web:da0a2b3934f07e9c6704aa",
-  measurementId: "G-L1ZRGFBZ1E"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
